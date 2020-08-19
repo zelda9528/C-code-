9a46de3d13fda0e9d623c6dc6b5adf30d684787f
heyuxuan <2671100876@qq.com>
2020-08-19 10:27:46 +0800

    输出图形HYX

---
+++ 8_19/8_19/源.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	printf("*  * *   * *   *\n");
+	printf("*  *  * *   * * \n");
+	printf("****   *     *  \n");
+	printf("*  *   *    * * \n");
+	printf("*  *   *   *   *\n");
+	return 0;
+}
