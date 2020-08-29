c17beea2ad5a92c0f9e5f52f679bd05cac72caf2
heyuxuan <2671100876@qq.com>
2020-08-29 14:34:30 +0800

    写一个递归函数DigitSum(n)，输入一个非负整数，返回组成它的数字之和

---
+++ 8_29_4/8_29_4/源.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int DigitSum(int n)
+{
+	if (n > 9)
+	{
+		return DigitSum(n / 10) + n % 10;
+	}
+	else
+	{
+		return n;
+	}
+}
+int main()
+{
+	
+	int n = 0;
+	scanf("%d", &n);
+	int ret = DigitSum(n);
+	printf("%d\n", ret);
+	return 0;
+}
