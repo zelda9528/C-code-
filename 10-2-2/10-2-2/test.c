2f7465cb615b3467bdbf3dfde6bcb2a753b8a1e2
heyuxuan <2671100876@qq.com>
2020-10-02 15:32:40 +0800

    数组越界访问实例

---
+++ 10-2-2/10-2-2/test.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	for (i = 0; i <= 12; i++)
+	{
+		arr[i] = 0;
+		printf("hello \n");
+	}
+	return 0;
+}
