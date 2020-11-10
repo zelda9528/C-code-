61bba0e37ec0f1434e0a35d56eb914229682f0f3
heyuxuan <2671100876@qq.com>
2020-11-10 21:27:32 +0800

    ascii码转字符

---
+++ 11-10-3/11-10-3/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+	int arr[150] = { 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116 , 33 };
+	for (int i = 0; i < 15; i++)
+	{
+		printf("%c", arr[i]);
+	}
+	return 0;
+}
