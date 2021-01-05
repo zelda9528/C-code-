a16d892732ea344ff5d4d7e40ad462ef5131ad61
heyuxuan <2671100876@qq.com>
2021-01-05 18:48:14 +0800

    。。

---
+++ 2021-1-5-2/2021-1-5-2/test.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+
+
+//Ò»Î¬Êý×é
+int a[] = { 1,2,3,4 };
+printf("%d\n", sizeof(a));
+printf("%d\n", sizeof(a + 0));
+printf("%d\n", sizeof(*a));
+printf("%d\n", sizeof(a + 1));
+printf("%d\n", sizeof(a[1]));
+printf("%d\n", sizeof(&a));
+printf("%d\n", sizeof(*&a));
+printf("%d\n", sizeof(&a + 1));
+printf("%d\n", sizeof(&a[0]));
+printf("%d\n", sizeof(&a[0] + 1));
+
+	return 0;
+}
