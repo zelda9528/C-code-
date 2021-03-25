b61df79aa604ba2f557398c8bc07339479aba136
heyuxuan <2671100876@qq.com>
2021-03-25 14:40:46 +0800

    string::insert

---
+++ 2021-3-25-2/2021-3-25-2/test.cpp
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main()
+{
+	string str = "mm";
+	string str2 = "123";
+	//string& insert(size_t pos, const string& str);
+	str.insert(1,str2);//m123m
+	cout << str << endl;
+
+	//string& insert(size_t pos, const string& str, size_t subpos, size_t sublen);
+	str.insert(1,str2,1,2);//m23123m
+	cout << str << endl;
+
+	//string& insert(size_t pos, const char* s);
+	str.insert(0,"abc");//abcm23123m
+	cout << str << endl;
+
+	//string& insert(size_t pos, const char* s, size_t n);
+	str.insert(0,"000000",5);//00000zbcm23123m
+	cout << str << endl;
+
+	/*string& insert(size_t pos, size_t n, char c);
+	void insert(iterator p, size_t n, char c);*/
+	str.insert(str.begin(),3,'x');//xxx00000zbcm23123m
+	cout << str << endl;
+
+	//iterator insert(iterator p, char c);
+	
+	/*template \<class InputIterator>
+		>> void insert(iterator p, InputIterator first, InputIterator last);*/
+	str.insert(str.end(),str2.begin(),str2.end());//xxx00000zbcm23123m123
+
+	cout << str << endl;
+
+
+}
