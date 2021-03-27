83f8e18e637e46e7e0564f79d5ddeb6ea2bf0e31
heyuxuan <2671100876@qq.com>
2021-03-27 16:05:05 +0800

    蓝桥杯竞赛题：小明体力：10000；跑一分钟-600，休息一分钟+300，问跑了几分钟？

---
+++ 2021-3-27-1/2021-3-27-1/test.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+int ans;
+//原则：先跑后休息 
+int main()
+{
+	int x = 10000;
+	while (x)
+	{
+		//如果我还能跑全程， 那就跑 
+		if (x - 600 >= 0)
+		{
+			ans = ans + 60 * 2;
+			x = x - 600 + 300;
+		}
+		else
+		{//如果我跑不了全程，那就跑多少算多少 。体力的损耗和增加都是均匀变化 
+			ans = ans + 0.1*x;
+			x = 0;
+		}
+	}
+	cout << ans << endl;
+	return 0;
+}
