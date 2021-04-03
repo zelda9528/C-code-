62e06e256f22eef72217f9ad451a30a272f12cfa
heyuxuan <2671100876@qq.com>
2021-04-03 16:20:52 +0800

    list常用接口

---
+++ list4.3/list4.3/list.cpp
@@ -0,0 +1,89 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<vector>
+#include<list>
+using namespace std;
+
+
+//构造接口
+//void test()
+//{
+//	list<char>lst1;
+//	list<int>lst2(4, 100);
+//	vector<char>vec;
+//	vec.push_back('a');
+//	vec.push_back('a');
+//	vec.push_back('a');
+//	vec.push_back('a');
+//	list<char>lst3(vec.begin(),vec.end());
+//
+//	list<int>lst4(lst2);
+//}
+
+//迭代器    范围for
+//void test()
+//{
+//	list<char>lst(4, 'x');
+//	list<char>::iterator it = lst.begin();
+//	for (const char&e : lst)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//}
+//void test()
+//{
+//	string s="0abcdef";
+//	list<char>lst(s.begin(),s.end());
+//
+//	list<char>::iterator it = lst.end();
+//	//end()指向的是最后一个元素的下一个位置，要先-1才指向最后一个元素
+//	--it;
+//	while (it != lst.begin())
+//	{
+//		cout << *it << " ";
+//		--it;
+//	}
+//	cout << endl;
+//
+//}
+
+//插入接口
+//void test()
+//{
+//	list<int>lst;
+//
+//	lst.push_back(9);
+//	lst.push_back(9);
+//	lst.push_back(9);
+//	lst.push_back(9);
+//	lst.push_back(9);
+//
+//	lst.push_front(0);
+//	lst.push_front(0);
+//	lst.push_front(0);
+//	lst.push_front(0);
+//	lst.push_front(0);
+//
+//	lst.insert(lst.begin(), 0);
+//	lst.insert(lst.end(), 9);
+//
+//
+//	lst.emplace(lst.begin(), 1);
+//
+//	
+//	
+//}
+
+
+void test()
+{
+
+}
+int main()
+{
+	test();
+	return 0;
+}
+
+
