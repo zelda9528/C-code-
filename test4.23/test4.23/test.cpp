d2eba78be224268460174953ec00047b98aeb5c5
heyuxuan <2671100876@qq.com>
2021-04-23 17:00:15 +0800

    test4.23

---
+++ test4.23/test4.23/test.cpp
@@ -0,0 +1,61 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+#include<queue>
+
+using namespace std;
+int main()
+{
+
+	priority_queue<int> a;
+
+	priority_queue<int, vector<int>, greater<int> > c;
+
+	priority_queue<string> b;
+
+	for (int i = 0; i < 5; i++)
+
+	{
+
+		a.push(i);
+
+		c.push(i);
+
+	}
+
+	while (!a.empty())
+
+	{
+
+		cout << a.top() << ' ';
+
+		a.pop();
+
+	}
+
+	cout << endl;
+
+	while (!c.empty())
+
+	{
+
+		cout << c.top() << ' ';
+
+		c.pop();
+
+	}
+
+	cout << endl;
+	b.push("abc");
+	b.push("abcd");
+	b.push("cbd");
+	while (!b.empty())
+	{
+		cout << b.top() << ' ';
+		b.pop();
+
+	}
+	cout << endl;
+	return 0;
+
+}
