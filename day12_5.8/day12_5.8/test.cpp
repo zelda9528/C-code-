f855fb817dc8d61bab9cbb1f94cd114f0137b6f9
heyuxuan <2671100876@qq.com>
2021-05-09 19:21:10 +0800

    day12_5.8

---
+++ day12_5.8/day12_5.8/test.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+
+using namespace std;
+
+//int main()
+//{
+//	int n[][3] = { 10,20,30,40,50,60 };
+//	int(*p)[3];
+//	p = n;
+//	cout << p[0][0] << " " << *(p[0] + 1) << " " << (*p)[2] << endl;
+//}
+
+//void func(const int &val1, const int &val2)
+//{
+//	cout << val1 << ' ';
+//	cout << val2 << ' ';
+//}
+//int main()
+//{
+//	int i = 0;
+//	func(++i, i++);
+//	return 0;
+//}
+
+//class MYclass{
+//
+//	~MYclass()
+//	{
+//		delete this;
+//		this = NULL;
+//	}
+//};
+//
+//int main()
+//{
+//	return 0;
+//}
+
+
+// 6 3 2
+//x/=y+z*y+z/y+z*y+z
+//x=
