c58bafc6ef8337272d92e3b2bb93ee99dba104e0
heyuxuan <2671100876@qq.com>
2021-05-26 09:38:00 +0800

    剑指offer。用两个栈实现队列

---
+++ stack--queue/stack--queue/test.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+//https://www.nowcoder.com/practice/54275ddae22f475981afa2244dd448c6?tpId=13&&tqId=11158&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+class Solution
+{
+public:
+	void push(int node) {
+		stack1.push(node);
+	}
+
+	int pop() {
+		if (stack2.empty())
+		{
+			while (!stack1.empty())
+			{
+				stack2.push(stack1.top());
+				stack1.pop();
+			}
+		}
+		int top = stack2.top();
+		stack2.pop();
+		return top;
+	}
+
+private:
+	stack<int> stack1;
+	stack<int> stack2;
+};
