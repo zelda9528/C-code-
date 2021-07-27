4bc930fd12a21ee7276c320fb65736b71dd7df13
heyuxuan <2671100876@qq.com>
2021-07-27 18:33:57 +0800

    test_7.27

---
+++ test_7.27/test_7.27/test.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+class A {
+	int _a;
+	static int b;
+	
+};
+int main()
+{
+	
+	cout << sizeof(A) << endl;
+	return 0;
+}
